92bd2030ca2fd335f395456b4c8192a318486747
mkhellou <yoyomcawom@gmail.com>
2023-01-14 13:24:32 +0100

    ----commit at date : Sat Jan 14 13:24:32 +01 2023-----

--- srcs/main.c
+++ srcs/main.c
@@ -6,49 +6,50 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 17:39:59 by mkhellou          #+#    #+#             */
-/*   Updated: 2023/01/14 13:15:51 by mkhellou         ###   ########.fr       */
+/*   Updated: 2023/01/14 13:23:39 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void exit_fuction(void)
+void	exit_fuction(void)
 {
 	exit(EXIT_FAILURE);
 }
 
-char ***total_generator(int argc, char **argv)
+char	***total_generator(int argc, char **argv)
 {
-	char ***total;
-	int i;
-	int j;
+	char	***total;
+	int		i;
+	int		j;
 
-	total= (char***)ft_calloc(argc +1, sizeof(char**)); 
+	total = (char ***)ft_calloc(argc + 1, sizeof(char **));
 	j = 1;
 	while (j < argc)
 	{
 		i = 0;
-		while(argv[j][i])
+		while (argv[j][i])
 		{
 			if (!ft_isdigit(argv[j][i]))
 			{
-				if(argv[j][i] == '+' || argv[j][i] == '-')
+				if (argv[j][i] == '+' || argv[j][i] == '-')
 				{
-					if (argv[j][i+1] == '+' || argv[j][i+1] == '-' || argv[j][i+1] == '\0')
+					if (argv[j][i + 1] == '+' || argv[j][i + 1] == '-'
+						|| argv[j][i + 1] == '\0')
 					{
 						ft_putstr_fd("Error", 2);
 						exit_fuction();
 					}
-					if(i != 0 && argv[j][i-1] != ' ')
+					if (i != 0 && argv[j][i - 1] != ' ')
 					{
 						ft_putstr_fd("Error", 2);
 						exit_fuction();
 					}
 				}
-				else if(argv[j][i] == ' ')
+				else if (argv[j][i] == ' ')
 				{
 					i++;
-					continue;
+					continue ;
 				}
 				else
 				{
@@ -64,11 +65,11 @@ char ***total_generator(int argc, char **argv)
 	return (total);
 }
 
-int total_counter(int argc,char ***total)
+int	total_counter(int argc, char ***total)
 {
-	int i;
-	int j;
-	int count;
+	int	i;
+	int	j;
+	int	count;
 
 	count = 0;
 	j = 1;
@@ -82,19 +83,19 @@ int total_counter(int argc,char ***total)
 		}
 		j++;
 	}
-	return(count);
+	return (count);
 }
 
-char **data_joiner(char*** total, int argc)
+char	**data_joiner(char ***total, int argc)
 {
-	int count;
-	char **final;
-	int i;
-	int j;
-	int c;
-	
-	count = total_counter(argc,total);
-	final = (char **)ft_calloc(count +1, sizeof(char**));
+	int		count;
+	char	**final;
+	int		i;
+	int		j;
+	int		c;
+
+	count = total_counter(argc, total);
+	final = (char **)ft_calloc(count + 1, sizeof(char **));
 	c = 0;
 	j = 1;
 	while (j < argc)
@@ -102,7 +103,7 @@ char **data_joiner(char*** total, int argc)
 		i = 0;
 		while (total[j][i])
 		{
-			final[c]=total[j][i];
+			final[c] = total[j][i];
 			c++;
 			i++;
 		}
@@ -110,9 +111,9 @@ char **data_joiner(char*** total, int argc)
 	}
 	return (final);
 }
-void free_total(char ***total,int argc)
+void	free_total(char ***total, int argc)
 {
-	int j;
+	int	j;
 
 	j = 1;
 	while (j < argc)
@@ -123,18 +124,17 @@ void free_total(char ***total,int argc)
 	free(total);
 }
 
-char **check_duplicates(char **final)
+char	**check_duplicates(char **final)
 {
-	int count;
-	char **dup;
-	int i;
-	int j;
+	int		count;
+	char	**dup;
+	int		i;
+	int		j;
 
 	count = 0;
 	while (final[count])
 		count++;
-	dup = (char **)ft_calloc(count +1, sizeof(char**));
-	
+	dup = (char **)ft_calloc(count + 1, sizeof(char **));
 	dup[0] = final[0];
 	i = 1;
 	while (final[i])
@@ -142,7 +142,8 @@ char **check_duplicates(char **final)
 		j = 0;
 		while (dup[j])
 		{
-			if (ft_strncmp(dup[j],final[i],ft_strlen(dup[j])+ft_strlen(final[i])) == 0)
+			if (ft_strncmp(dup[j], final[i], ft_strlen(dup[j])
+					+ ft_strlen(final[i])) == 0)
 			{
 				ft_putstr_fd("Error", 2);
 				exit_fuction();
@@ -156,57 +157,55 @@ char **check_duplicates(char **final)
 	return (dup);
 }
 
-int **numbers_converter(char **final)
+int	*numbers_converter(char **final)
 {
-	int i;
-	int *tab;
+	int	i;
+	int	*tab;
 	int	check;
 
 	check = 0;
 	i = 0;
 	while (final[i])
 		i++;
-	tab= (int*)ft_calloc(i, sizeof(int));
+	tab = (int *)ft_calloc(i + 1, sizeof(int));
 	i = 0;
 	while (final[i])
 	{
 		tab[i] = ft_atoi_max(final[i], &check);
-		if(check == 1)
+		if (check == 1)
 		{
 			ft_putstr_fd("Error", 2);
 			exit_fuction();
 		}
-		if(check == -1)
+		if (check == -1)
 		{
 			ft_putstr_fd("Error", 2);
 			exit_fuction();
 		}
-		ft_printf("%d\n",tab[i]);
 		i++;
 	}
-	return (0);
+	return (tab);
 }
 
 //garbage collector system
 int	main(int argc, char **argv)
 {
-	char ***total;
-	char **final;
-	int **tab;
+	char	***total;
+	char	**final;
+	int		*tab;
+	int		i;
 
 	if (argc == 1)
-	{
 		return (0);
-	}
 	total = total_generator(argc, argv);
-	final = data_joiner(total,argc);
-	free_total(total,argc);
-//	int i = 0;
+	final = data_joiner(total, argc);
+	free_total(total, argc);
+	i = 0;
 	final = check_duplicates(final);
 	tab = numbers_converter(final);
-	// while (final[i])
-	// {
-	// 	ft_printf("%s\n", final[i]);
-	// 	i++;
-	// }
+	while (tab[i])
+	{
+		ft_printf("%d\n", tab[i]);
+		i++;
+	}
 }
