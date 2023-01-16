c578ac2ed77eb6f596d5126b3084544c87e0b6d5
mkhellou <yoyomcawom@gmail.com>
2023-01-16 19:18:48 +0100

    ----commit at date : Mon Jan 16 19:18:48 +01 2023-----

--- srcs/3-sorting_system.c
+++ srcs/3-sorting_system.c
@@ -6,7 +6,7 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 16:19:02 by mkhellou          #+#    #+#             */
-/*   Updated: 2023/01/16 18:01:45 by mkhellou         ###   ########.fr       */
+/*   Updated: 2023/01/16 18:10:23 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,46 +21,32 @@ int	get_index(int p, t_stack *sta)
 		i++;
 	return(i);
 }
+
 void	sorting_system(t_stack *sta, t_stack *stb)
 {
 	int i;
-	// int len;
-	// int j;
+	int len;
+	int j;
 
-	// len = sta->size;
-	// j = 0;
-	(void)stb;
-	i = 0;
-	while (i < sta->size)
+	len = sta->size;
+	j = 0;
+	while (j < len)
 	{
-		ft_printf("%d-%d\n",get_index(i, sta), sta->stack[get_index(i, sta)].nbr);
-		i++;
+		i = 0;
+		while (i < sta->size)
+		{
+			if (sta->stack[0].pos != j)
+				move(ra, sta, stb);
+			i++;
+		}
+		move(pb, sta, stb);
+		j++;
 	}
-	
-	
-	// while (j < len)
-	// {
-	// 	i = 0;
-	// 	while (i < sta->size)
-	// 	{
-	// 		//ft_printf("%d-%d\n",j,sta->stack[i].pos);
-	// 		if (sta->stack[0].pos != j)
-	// 			move(ra, sta, stb);
-	// 		i++;
-	// 	}
-	// 	move(pb, sta, stb);
-	// 	j++;
-	// }
-	// j = 0;
-	// while (j < len)
-	// {
-	//move(ra, sta, stb);
-	//ft_printf("%d",sta->stack[0].nbr);
-	// 	j++;
-	// }
-	
-	// move(sa, sta, stb);
-	// move(rra, sta, stb);
-	// move(rra, sta, stb);
+	j = 0;
+	while (j < len)
+	{
+		move(pa, sta, stb);
+		j++;
+	}
 	return;
 }
