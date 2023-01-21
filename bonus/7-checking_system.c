b4e346d77c4eecc9135561d9c88ab1797fd750d1
mkhellou <yoyomcawom@gmail.com>
2023-01-21 19:56:17 +0100

    ----commit at date : Sat Jan 21 19:56:17 +01 2023-----

--- bonus/7-checking_system.c
+++ bonus/7-checking_system.c
@@ -6,7 +6,7 @@
 /*   By: mkhellou < mkhellou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 10:14:59 by mkhellou          #+#    #+#             */
-/*   Updated: 2023/01/21 19:18:51 by mkhellou         ###   ########.fr       */
+/*   Updated: 2023/01/21 19:55:32 by mkhellou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,84 +42,50 @@ int sorted_check(t_stack *sta)
 void cheking_system(t_stack *sta, t_stack *stb)
 {
 	char *str;
-	char *all;
-	char **instruct;
-	(void)sta;
+	//int sorted;
 	(void)stb;
-
-	all = "";
-	while (1)
+	(void)sta;
+	str =get_next_line(0);
+	while (str)
 	{
+		// if (ft_strlen(str) != 4 && ft_strlen(str) != 3)
+		// {
+		// 	ft_printf("Error\n");
+		// 	exit(1);
+		// }
+		// if (ft_strncmp(str,"sa\n",3) == 0)
+		// 	move(sa,sta,stb);
+		// else if (ft_strncmp(str,"sb\n",3) == 0)
+		// 	move(sb,sta,stb);	
+		// else if (ft_strncmp(str,"ss\n",3) == 0)
+		// 	move(ss,sta,stb);
+		// else if (ft_strncmp(str,"pa\n",3) == 0)
+		// 	move(pa,sta,stb);
+		// else if (ft_strncmp(str,"pb\n",3) == 0)
+		// 	move(pb,sta,stb);
+		// else if (ft_strncmp(str,"ra\n",3) == 0)
+		// 	move(ra,sta,stb);
+		// else if (ft_strncmp(str,"rb\n",3) == 0)
+		// 	move(rb,sta,stb);
+		// else if (ft_strncmp(str,"rr\n",3) == 0)
+		// 	move(rr,sta,stb);
+		// else if (ft_strncmp(str,"rra\n",4) == 0)
+		// 	move(rra,sta,stb);
+		// else if (ft_strncmp(str,"rrb\n",4) == 0)
+		// 	move(rrb,sta,stb);
+		// else if (ft_strncmp(str,"rrr\n",4) == 0)
+		// 	move(rrr,sta,stb);
+		// else
+		// {
+		// 	ft_printf("Error\n");
+		// 	exit(1);
+		// }
 		str =get_next_line(0);
-		if (!str)
-			break;
-		if (ft_strlen(str) != 4 && ft_strlen(str) != 3)
-		{
-			ft_printf("Error\n");
-			exit(1);
-		}
-		if (ft_strncmp(str,"sa\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"sb\n",5) == 0)
-			all =ft_strjoin(all,str);	
-		else if (ft_strncmp(str,"ss\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"pa\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"pb\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"ra\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"rb\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"rr\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"rra\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"rrb\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else if (ft_strncmp(str,"rrr\n",5) == 0)
-			all =ft_strjoin(all,str);
-		else
-		{
-			ft_printf("Error\n");
-			exit(1);
-		}
+		//ft_printf("%s",str);
 	}
-	instruct = ft_split(all,'\n');
-	int i;
-	i = 0;
-	while (instruct[i])
-	{
-		str = instruct[i];
-		if (ft_strncmp(str,"sa",5) == 0)
-			move(sa,sta,stb);
-		else if (ft_strncmp(str,"sb",5) == 0)
-			move(sb,sta,stb);	
-		else if (ft_strncmp(str,"ss",5) == 0)
-			move(ss,sta,stb);
-		else if (ft_strncmp(str,"pa",5) == 0)
-			move(pa,sta,stb);
-		else if (ft_strncmp(str,"pb",5) == 0)
-			move(pb,sta,stb);
-		else if (ft_strncmp(str,"ra",5) == 0)
-			move(ra,sta,stb);
-		else if (ft_strncmp(str,"rb",5) == 0)
-			move(rb,sta,stb);
-		else if (ft_strncmp(str,"rr",5) == 0)
-			move(rr,sta,stb);
-		else if (ft_strncmp(str,"rra",5) == 0)
-			move(rra,sta,stb);
-		else if (ft_strncmp(str,"rrb",5) == 0)
-			move(rrb,sta,stb);
-		else if (ft_strncmp(str,"rrr",5) == 0)
-			move(rrr,sta,stb);
-		i++;
-	}
-	int sorted;
-	sorted = sorted_check(sta);
-	if (sorted == 1)
-		ft_printf("OK\n");
-	else if (sorted == -1)
-		ft_printf("KO\n");
+	// sorted = sorted_check(sta);
+	// if (sorted == 1)
+	// 	ft_printf("OK\n");
+	// else if (sorted == -1)
+	// 	ft_printf("KO\n");
 }
